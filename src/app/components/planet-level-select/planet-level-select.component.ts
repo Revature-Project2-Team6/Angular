9003a988a39924f5cdf560eaf17cb0df33360cf0
Kyle Tumlinson <ktumli@yahoo.com>
2022-07-13 12:48:36 -0500

    Created components for Galaxy map, Planet map, and Planet encounters

---
+++ src/app/components/planet-level-select/planet-level-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planet-level-select',
+  templateUrl: './planet-level-select.component.html',
+  styleUrls: ['./planet-level-select.component.css']
+})
+export class PlanetLevelSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/planet-select/planet-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planet-select',
+  templateUrl: './planet-select.component.html',
+  styleUrls: ['./planet-select.component.css']
+})
+export class PlanetSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
