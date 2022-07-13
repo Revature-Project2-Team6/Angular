5d50f3e1d691dc5034a8a13e38395d2b2c37a6af
Kyle Tumlinson <ktumli@yahoo.com>
2022-07-13 17:39:10 -0500

    Finished most of the combat logic

--- src/app/components/combat/combat.component.ts
+++ src/app/components/combat/combat.component.ts
@@ -1,3 +1,4 @@
+import { Character } from './../../models/character';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CombatComponent implements OnInit {
 
-  constructor() { }
+  characterSelected: boolean = false;
+  statsUpdated: boolean = false;
+  combatCompleted: boolean = false;
+  constructor(private player: Character, private npc:Character) { }
 
+  winner: string = '';
   ngOnInit(): void {
   }
 
+  startFight(): void{
+    // display the stats
+    this.addSkillMods(this.player);
+    this.addSkillMods(this.npc);
+    this.statsUpdated = true;
+    this.winner = this.calculateWinner(this.player, this.npc);
+    this.subSkillMods(this.player);
+    this.subSkillMods(this.npc);
+    this.combatCompleted = true;
+  }
+
+  addSkillMods(char:Character){
+    this.useSkills(char);
+  }
+
+  subSkillMods(char:Character){
+    for(let skill = 0; skill < char.skills.length; skill++){
+      char.setStat(char.skills[skill].stat, -char.skills[skill].power);
+    }
+  }
+
+  useSkills(char:Character){
+    for(let skill = 0; skill < char.skills.length; skill++){
+      char.setStat(char.skills[skill].stat, char.skills[skill].power);
+    }
+  }
+
+  calculateWinner(player:Character, npc:Character) : string{
+    let playerEP: Number = player.stats.str + player.stats.fp;
+    let playerED: Number = player.stats.def + player.stats.fp;
+    let npcEP: Number = npc.stats.str + npc.stats.fp;
+    let npcED:Number = npc.stats.def + npc.stats.fp;
+
+    if(playerEP > npcED){
+      return 'player';
+    }else if(npcEP > playerED){
+      return 'npc';
+    }else{
+      return 'draw';
+    }
+  }
 }
--- src/app/models/character.ts
+++ src/app/models/character.ts
@@ -1,3 +1,5 @@
+import { User } from "./users";
+
 export class Character {
   id: number;
   name: string;
@@ -24,4 +26,102 @@ export class Character {
     this.skills = skills
     this.owner = owner
   }
+
+  setStat(stat: string, amount: number){
+    switch(stat){
+      case 'dex':{
+          this.stats.dex += amount;
+          break;
+        }
+        case 'def':{
+          this.stats.def += amount;
+          break;
+        }
+        case 'fp':{
+          this.stats.fp += amount;
+          break;
+        }
+        case 'str':{
+          this.stats.str += amount;
+          break;
+        }
+    }
+  }
+}
+
+
+export class Skill{
+  id: Number;
+  description: string;
+  name: string;
+  power: number;
+  stat: string;
+
+
+  constructor(
+    id: Number,
+    description: string,
+    name: string,
+    power: number,
+    stat: string
+) {
+    this.id = id
+    this.description = description
+    this.name = name
+    this.power = power
+    this.stat = stat
+  }
+
+}
+
+export class Stats{
+  id: number;
+  def: number;
+  dex: number;
+  exp: number;
+  fp: number;
+  health: number;
+  lvl: number;
+  str: number;
+  character_id;
+
+  constructor(
+    id: number,
+    def: number,
+    dex: number,
+    exp: number,
+    fp: number,
+    health: number,
+    lvl: number,
+    str: number,
+    character_id: number
+) {
+    this.id = id
+    this.def = def
+    this.dex = dex
+    this.exp = exp
+    this.fp = fp
+    this.health = health
+    this.lvl = lvl
+    this.str = str
+    this.character_id = character_id
+  }
+}
+
+export class Species{
+  id: number;
+  description: string;
+  name: string;
+
+
+  constructor(
+    id: number,
+    description: string,
+    name: string
+) {
+    this.id = id
+    this.description = description
+    this.name = name
+  }
+
 }
